a16e425d923c5ce7294736eccf80195305e3b28b
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2022-11-23 14:34:56 +0900

    CSS UPdate

--- components/projects/ProjectItem.js
+++ components/projects/ProjectItem.js
@@ -14,60 +14,63 @@ export default function ProjectItem(props) {
     <div className={classes.card}>
       <li className={classes.item}>
         <Card>
-          <div className={classes.image_test}>
-            <Image
-              className={classes.project_image}
-              alt=" "
-              // width={300}
-              // height={300}
-              // layout="fill"
-              // objectFit="cover"
-              src={
-                (props.title === "Flashcard-App" && require("/public/images/FlashcardApp-900.png")) ||
-                (props.title === "Dice-Game" && require("/public/images/DiceGame_2-900.png")) ||
-                (props.title === "Bike-Website" && require("/public/images/bicycle_site-3.png"))
-              }
-            />
-          </div>
-          <div className={classes.content_container}>
+          <Link href={`../projects/${props.title}`}>
+            <div className={classes.card_content}>
+              <div className={classes.content_container}>
+                <div className={classes.card_title}>
+                  <h1>{props.title}</h1>
+                </div>
+              </div>
+              <div className={classes.image_test}>
+                <Image
+                  className={classes.project_image}
+                  alt=" "
+                  width="100%"
+                  height="100%"
+                  layout="fill"
+                  // objectFit="cover"
+                  src={
+                    (props.title === "Flashcard-App" && require("/public/images/Flashcard_Project_image_2.png")) ||
+                    (props.title === "Dice-Game" && require("/public/images/DiceGame_2-900.png")) ||
+                    (props.title === "Bike-Website" && require("/public/images/bicycle_site-3.png"))
+                  }
+                />
+              </div>
+            </div>
+            {/* If i move the link to here the whole thing should work as  href when clicked */}
+
+            {/* <div className={classes.content_container}>
             {props.isClicked ? (
               <div className={classes.image_container}>
-                {/* <Image
-                src={require("/public/images/DiceGame_2-900.png")}
-                src={require(props.title === "Dice-Game" && "/public/images/DiceGame_2-900.png")}
-                I could put an ternary to check title and hardcode image in
-                alt={props.title}
-                objectFit="cover"
-                objectPosition="top center"
-                className={classes.image}
-                layout="fill"
-                /> */}
+                <Image
+                  src={require("/public/images/DiceGame_2-900.png")}
+                  src={require(props.title === "Dice-Game" && "/public/images/DiceGame_2-900.png")}
+                  I
+                  could
+                  put
+                  an
+                  ternary
+                  to
+                  check
+                  title
+                  and
+                  hardcode
+                  image
+                  in
+                  alt={props.title}
+                  objectFit="cover"
+                  objectPosition="top center"
+                  className={classes.image}
+                  layout="fill"
+                />
               </div>
             ) : (
               <Link href={`../projects/${props.title}`}>
                 <div className={classes.card_title}> {props.title}</div>
               </Link>
             )}
-            {/* <div>
-              <div className={classes.image_test}>
-                <Image
-                  alt=" "
-                  width={100}
-                  height={100}
-                  src={
-                    (props.title === "Flashcard-App" && require("/public/images/FlashcardApp-900.png")) ||
-                    (props.title === "Dice-Game" && require("/public/images/DiceGame_2-900.png")) ||
-                    (props.title === "Bike-Website" && require("/public/images/bicycle_site-2.png"))
-                  }
-                />
-              </div>
-            </div> */}
-
-            {/* <p>{props.content}</p> */}
-            {/* <div className={classes.actions}>
-              <button>Show Details</button>
-            </div> */}
-          </div>
+          </div> */}
+          </Link>
         </Card>
       </li>
     </div>
