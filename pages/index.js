0c5269d01281adca24f128861622f5827d300369
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-16 10:47:18 +0900

    Added Favicon

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
 // import React from "react"; - this is autoadded in next.js
 import { Link } from "react-scroll";
-
 import Navigation from "../components/sections/Navigation";
 import React from "react";
 import { Fragment } from "react";
 import classes from "./Homepage.module.css";
 import ProjectList from "../components/projects/ProjectList";
 import About from "../components/sections/About";
+import Head from "next/head";
 
 export default function Homepage() {
   const [returnBtn, setReturnBtn] = React.useState(false);
   console.log(returnBtn);
 
   return (
-    <div className={classes.container}>
-      <Navigation returnBtn={returnBtn} />
-      <About />
-      <ProjectList />
-    </div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={classes.container}>
+        <Navigation returnBtn={returnBtn} />
+        <About />
+        <ProjectList />
+      </div>
+    </>
   );
 }
